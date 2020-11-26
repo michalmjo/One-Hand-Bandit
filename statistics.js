911ded89801ec1a16fd7f7a410b58f2e5b9372dc
Michał Ożóg <m.ozog@protonmail.com>
2020-11-26 12:18:33 +0100

    add statistics

---
+++ statistics.js
@@ -0,0 +1,22 @@
+class Statistics {
+    constructor() {
+        this.gameResults = [];
+    }
+
+    addGameToStatistics(win, bid) {
+        let gameResult = {
+            win,
+            bid,
+        }
+        this.gameResults.push(gameResult);
+    }
+
+    showGameStats() {
+        let games = this.gameResults.length;
+        let wins = this.gameResults.filter(check => check.win).length
+        let losses = this.gameResults.filter(check => !check.win).length
+        return [games, wins, losses]
+    }
+}
+
+const stats = new Statistics()
